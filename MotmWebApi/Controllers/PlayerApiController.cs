18c8d6ee4c0742cb0800e28365b2ebcb152cd4d3
Sadri TBA <sadri_albaner@hotmail.com>
2018-05-15 09:35:27 +0200

    WEBAPI commit

---
+++ MotmWebApi/Controllers/PlayerApiController.cs
@@ -0,0 +1,63 @@
+﻿using System.Web.Http;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using MotmWebApi.Models;
+
+namespace MotmWebApi.Controllers
+{
+    public class PlayerApiController : ApiController
+    {
+        // GET: api/PlayerApi
+        public List<Player> Get()
+        {
+            var listOfPlayers = new List<Player>();
+
+            using (var connection = new MySqlConnection("user id=alphajob_org;" +
+                           "password=Q1fshaXh0rgxhGj1n7;server=mysql49.unoeuro.com;" +
+                           "database=alphajob_org_db;" +
+                           "connection timeout=30"))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Player ORDER BY playerId ASC LIMIT 11";
+                using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var p = new Player();
+                            p.playerId = (int)reader["playerId"];
+                            p.playerName = reader["playerName"].ToString();
+                            p.playerNr = (int)reader["playerNr"];
+                            p.position = reader["position"].ToString();
+                            listOfPlayers.Add(p);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return listOfPlayers;
+        }
+
+        // GET: api/PlayerApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PlayerApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PlayerApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PlayerApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MotmWebApi/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotmWebApi.Models
+{
+    public class Player
+    {
+        public int playerId { get; set; }
+        public string playerName { get; set; }
+        public int playerNr { get; set; }
+        public string position { get; set; }
+    }
+}
